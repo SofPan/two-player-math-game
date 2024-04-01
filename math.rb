c4451cdc81403dafbf9bb16e88a3314bf5f57d51
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-04-01 16:13:17 -0400

    check if current player guess is correct and swap players after guess

--- math.rb
+++ math.rb
@@ -6,9 +6,10 @@ end
 
 class Game
   include Helpers
-  attr_accessor :winning_answer, :game_won
+  attr_accessor :winning_answer, :game_won, :current_player, :other_player
   def initialize(players)
     @current_player = players[0]
+    @other_player = players[1]
     @current_score = "player1: #{players[0].lives}/3 vs. player2: #{players[1].lives}/3"
     @winning_answer = 0
     @game_won = false
@@ -22,13 +23,17 @@ class Game
 
   def game_loop
     puts generate_question
+    round_won = false
     until game_won
+      player_turn = current_player
       player_answer = gets.to_i
       # For TESTING, remove later
-      puts "player answer is #{player_answer}, the correct answer is #{winning_answer}"
-      if player_answer == winning_answer
-        self.game_won= true
-      end
+      is_correct = player_answer == winning_answer
+
+      puts "#{current_player.name}'s answer is #{player_answer}, they are #{is_correct ? "right!" : "totally wrong!"}"
+
+      self.current_player= other_player
+      self.other_player= player_turn
     end
     # For TESTING, remove later
     puts "exiting game loop"
@@ -36,11 +41,15 @@ class Game
 end
 
 class Player
-  attr_accessor :lives
+  attr_accessor :lives, :name
   def initialize(name)
     @name = name
     @lives = 3
     @is_alive = true
+  end
+
+  def lose_a_life
+    @lives -= 1
   end
 end
 
