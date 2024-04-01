39e2cc2ee31ea66929e6c4857b9b1a5adbdc71e7
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-04-01 15:34:18 -0400

    create Helpers Module and Game Class

--- math.rb
+++ math.rb
@@ -1,3 +1,24 @@
+module Helpers
+  def self.random_number
+    [rand(1...20), rand(1...20)]
+  end
+end
+
+class Game
+  include Helpers
+  attr_accessor :winning_answer
+  def initialize
+    @current_player = "player1"
+    @current_score = "player1: #{player1.lives}/3 vs. player2: #{player2.lives}/3"
+    @winning_answer = 0
+  end
+  def generate_question
+    values = Helpers.random_number
+    self.winning_answer= values[0] + values[1] 
+    "#{values[0]} + #{values[1]} = ?"
+  end
+end
+
 class Player
   attr_accessor :lives
   def initialize(name)
