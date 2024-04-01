097471506f95815462d8e70ef68ad863146688b7
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-04-01 15:57:12 -0400

    define game_loop function and set up to take user input

--- math.rb
+++ math.rb
@@ -6,16 +6,32 @@ end
 
 class Game
   include Helpers
-  attr_accessor :winning_answer
-  def initialize
-    @current_player = "player1"
-    @current_score = "player1: #{player1.lives}/3 vs. player2: #{player2.lives}/3"
+  attr_accessor :winning_answer, :game_won
+  def initialize(players)
+    @current_player = players[0]
+    @current_score = "player1: #{players[0].lives}/3 vs. player2: #{players[1].lives}/3"
     @winning_answer = 0
+    @game_won = false
   end
+
   def generate_question
     values = Helpers.random_number
     self.winning_answer= values[0] + values[1] 
     "#{values[0]} + #{values[1]} = ?"
+  end
+
+  def game_loop
+    puts generate_question
+    until game_won
+      player_answer = gets.to_i
+      # For TESTING, remove later
+      puts "player answer is #{player_answer}, the correct answer is #{winning_answer}"
+      if player_answer == winning_answer
+        self.game_won= true
+      end
+    end
+    # For TESTING, remove later
+    puts "exiting game loop"
   end
 end
 
@@ -30,3 +46,7 @@ end
 
 player1 = Player.new("player1")
 player2 = Player.new("player2")
+
+game = Game.new([player1, player2])
+
+game.game_loop
